3297c948dd17521a4f0b1e6ee59e47f7755327dc
Alice Abarca <aliceabarca@otmail.com>
2023-06-08 15:23:50 -0600

    Add an id instead of a class to the win count element and add the create player function

--- main.js
+++ main.js
@@ -4,5 +4,16 @@ var gridItems = document.querySelectorAll('.grid-item');
 var playersTurnHeading = document.querySelector('.players-turn');
 var playerOneWinCount = document.querySelector('.player-1-container .win-count');
 var playerTwoWinCount = document.querySelector('.player-2-container .win-count');
+var ticTacToeGrid = documen.querySelector('.tic-tac-toe-grid');
+
+ticTacToeGrid.addEventListener('click',)
 
 
+function createPlayer(player, token, id) {
+  return {
+    player: player,
+    token: token,
+    id: id,
+    win: false
+  }
+}
