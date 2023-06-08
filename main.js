5b6dc9ef352de18b7fdd0be22e738631cf29b9dc
Alice Abarca <aliceabarca@otmail.com>
2023-06-08 11:35:12 -0600

    Add query selectors to my main js file

---
+++ main.js
@@ -0,0 +1,8 @@
+var playerOneContainer = document.querySelector('.player-1-container');
+var playerTwoContainer = document.querySelector('.player-2-container');
+var gridItems = document.querySelectorAll('.grid-item');
+var playersTurnHeading = document.querySelector('.players-turn');
+var playerOneWinCount = document.querySelector('.player-1-container .win-count');
+var playerTwoWinCount = document.querySelector('.player-2-container .win-count');
+
+
