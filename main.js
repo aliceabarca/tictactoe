8ef39c7ac2538e4e26e78467d78f7d048de92da3
Alice Abarca <aliceabarca@otmail.com>
2023-06-08 18:15:57 -0600

    Add a random player to start game on page load

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ var gridItems = document.querySelectorAll('.grid-item');
 var playersTurnHeading = document.querySelector('.players-turn');
 var playerOneWinCount = document.querySelector('.player-1-container .win-count');
 var playerTwoWinCount = document.querySelector('.player-2-container .win-count');
-var ticTacToeGrid = documen.querySelector('.tic-tac-toe-grid');
+var ticTacToeGrid = document.querySelector('.tic-tac-toe-grid');
+var currentPlayer;
 
-ticTacToeGrid.addEventListener('click',)
+var emojis = ['🥸', '🤖']
 
+
+ticTacToeGrid.addEventListener('click', checkBox);
+window.addEventListener('load', displayTurn)
 
 function createPlayer(player, token, id) {
   return {
@@ -16,4 +20,33 @@ function createPlayer(player, token, id) {
     id: id,
     win: false
   }
+}
+
+function getRandomEmoji() {
+  var randomIndex = Math.floor(Math.random() * emojis.length);
+  return emojis[randomIndex];
+}
+
+function displayTurn() {
+  for (var i = 0; i < emojis.length; i++) {
+    playersTurnHeading.innerText = `It's ${[getRandomEmoji()]}'s turn`
+  }
+}
+
+function checkBox(event) {
+  console.log(event.target)
+}
+
+function winGame() { 
+  var winCombo = [
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [7, 5, 3],
+    [1, 5, 9]
+  ];
+  return winCombo;
 }
