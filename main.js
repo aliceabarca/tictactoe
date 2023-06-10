5a2988e3cd088086bece0094aa338cefa5e4f11e
Alice Abarca <aliceabarca@otmail.com>
2023-06-10 13:46:34 -0600

    Edit the main js and html to clean up

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ var playerTwoWinCount = document.querySelector('.player-2-container .win-count')
 var ticTacToeGrid = document.querySelector('.tic-tac-toe-grid');
 var row = document.querySelectorAll('.row');
 
-// var emojis = ['🥸', '🤖']
-
 var player1 = createPlayer('🥸', 'player 1', 0);
 var player2 = createPlayer('🤖', 'player 2', 0);
 var players = [player1, player2];
@@ -18,7 +16,6 @@ var gameBoard = ['', '', '', '', '', '', '', '', ''];
 ticTacToeGrid.addEventListener('click', checkBox);
 ticTacToeGrid.addEventListener('click', updateGame)
 window.addEventListener('load', displayTurn)
-// row.addEventListener('click', updateGame)
 
 function checkBox(event) {
   var target = event.target.id
@@ -26,9 +23,6 @@ function checkBox(event) {
     currentPlayer = (currentPlayer === player1) ? player2 : player1;
     playersTurnHeading.innerText = `It's ${currentPlayer.token}'s turn.`;
   }
-  // updateGame()
-  // console.log(currentPlayer.token)
-  // console.log(target === player1.token)
 }
 
 function winGame() { 
@@ -46,38 +40,26 @@ function winGame() {
 }
 
 function updateGame(event) {
-  // checkBox()
-  // var box = parseInt(event.target.closest('.grid-item').id)
   var box = Array.from(gridItems).indexOf(event.target);
   console.log(box)
-  // gameBoard[box] = currentPlayer.token
   if (gameBoard[box] === '') {
     gameBoard[box] = currentPlayer.token;
     event.target.textContent = currentPlayer.token;
-    // currentPlayer = (currentPlayer === player1) ? player2 : player1;
   }
 }
-// console.log(gridItems)
 
 function switchTurn() {
-  // if (player1.isTurn === false) {
-  //   // player1.isTurn = true;
-  //   displayTurn()
-  //   player2.isTurn = true;
-  // }
   player1.isTurn = !player1.isTurn;
   player2.isTurn = !player2.isTurn;
 }
 
 function displayTurn() {
-  // switchTurn()
   if (player1.isTurn === false) {
     playersTurnHeading.innerText = `It's ${player1.token}'s turn.`;
   } else {
     playersTurnHeading.innerText = `It's ${player2.token}'s turn.`
   }
 }
-
 
 function getRandomPlayer() {
   var randomIndex = Math.floor(Math.random() * players.length);
